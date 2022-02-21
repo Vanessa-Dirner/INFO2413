44a72bf824fe234d8590229f03d0fbb86e3a74e9
Vanessa Dirner <vanessa.dirner@email.kpu.ca>
2022-02-21 11:58:02 -0800

    Update projectdb.sql

--- projectdb.sql
+++ projectdb.sql
@@ -15,16 +15,28 @@ CREATE TABLE inventory (
     PRIMARY KEY (batch)
 );
 
+ insert into inventory (batch, expirationdate, quantity, seedtype, seedname, plantingtime)
+  values ('B0011', '2023-01-14'  ,'05000'  , 'Sunflower', 'Titan', 'Spring' ),
+         ('B0012', '2022-07-22'  ,'10000'  , 'Tomato', 'Power Pops', 'Spring'),
+         ('B0013', '2024-01-10'  ,'05000'  , 'Apple', 'Captain Kidd', 'Spring'),
+         ('B0014', '2023-12-23'  ,'05000'  , 'Grape', 'Moon Drops', 'Spring'),
+         ('B0015', '2022-04-20'  ,'05000'  , 'Blueberry', 'Top Hat', 'Winter');
 
 CREATE TABLE waste (
 	batch CHAR(4) NOT NULL,
-    seedtype CHAR(10) NOT NULL,
-    seedname VARCHAR(15) NOT NULL ,
-    quantity VARCHAR(9) NOT NULL ,
     expirationdate DATE NOT NULL ,
+    quantity VARCHAR(5) NOT NULL ,
+    seedtype CHAR(15) NOT NULL,
+    seedname CHAR(20) NOT NULL
     PRIMARY KEY (batch)
 );
  
+  insert into waste (batch, expirationdate, quantity, seedtype, seedname)
+  values ('B0001', '2020-01-10'  ,'00125'  , 'Pear', 'Bosc', 'Winter' ),
+         ('B0002', '2019-07-22'  ,'00608'  , 'Rhubarb', 'KangaRhu', 'Spring'),
+         ('B0003', '2021-01-10'  ,'00005'  , 'Celery', 'Conquistador', 'Winter'),
+         ('B0004', '2022-01-01'  ,'00473'  , 'Lemon', 'Eureka', 'Spring'),
+         ('B0005', '2018-09-09'  ,'00201'  , 'Lemon', 'Baboon', 'Spring');
 
 CREATE TABLE vendor (
     batch CHAR(4) NOT NULL,
@@ -36,6 +48,15 @@ CREATE TABLE vendor (
     PRIMARY KEY (batch, vendorname)
 );
 
+
+  insert into vendor (batch, expirationdate, vendorname, seedtype, seedname, quantityavailable)
+  values ('B0001', '2020-01-10'  ,'00125'  , 'Pear', 'Bosc', 'Winter' ),
+         ('B0002', '2019-07-22'  ,'00608'  , 'Rhubarb', 'KangaRhu', 'Spring'),
+         ('B0003', '2021-01-10'  ,'00005'  , 'Celery', 'Conquistador', 'Winter'),
+         ('B0004', '2022-01-01'  ,'00473'  , 'Lemon', 'Eureka', 'Spring'),
+         ('B0005', '2018-09-09'  ,'00201'  , 'Lemon', 'Baboon', 'Spring');
+
+         
 
 CREATE TABLE staff (
     userid CHAR(6) NOT NULL,
@@ -63,13 +84,8 @@ CREATE TABLE staff (
   show create table vendor;
  
   -- ------------------Fill the Tables------------------
-  insert into inventory (batch, expirationdate, quantity, seedtype, seedname, plantingtime)
-  values ('B0001', '2022-01-10'  ,'05000'  , 'Sunflower', 'Titan', 'Spring' ),
-         ('B0002', '2021-07-22'  ,'10000'  , 'Tomato', 'Power Pops', 'Spring'),
-         ('B0003', '2022-01-10'  ,'05000'  , 'Apple', 'Captain Kidd', 'Spring'),
-         ('B0004', '2022-01-10'  ,'05000'  , 'Grape', 'Moon Drops', 'Spring'),
-         ('B0005', '2022-01-10'  ,'05000'  , 'Blueberry', 'Top Hat', 'Winter');
  
  
+
 
  drop database project;
