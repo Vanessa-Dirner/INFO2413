2d0730a05db3eed43a41769db447aa6ab313d30a
Vanessa Dirner <vanessa.dirner@email.kpu.ca>
2022-02-21 18:05:43 -0800

    Update projectdb.sql

--- projectdb.sql
+++ projectdb.sql
@@ -23,20 +23,22 @@ CREATE TABLE inventory (
          ('B0015', '2022-04-20'  ,'05000'  , 'Blueberry', 'Top Hat', 'Winter');
 
 CREATE TABLE waste (
-	batch CHAR(4) NOT NULL,
+    wasteid CHAR(5) NOT NULL, 
+  	batch CHAR(5) NOT NULL,
     expirationdate DATE NOT NULL ,
     quantity VARCHAR(5) NOT NULL ,
     seedtype CHAR(15) NOT NULL,
-    seedname CHAR(20) NOT NULL
-    PRIMARY KEY (batch)
-);
+    seedname CHAR(20) NOT NULL,
+    PRIMARY KEY (wasteid)
+    );
  
-  insert into waste (batch, expirationdate, quantity, seedtype, seedname)
-  values ('B0001', '2020-01-10'  ,'00125'  , 'Pear', 'Bosc', 'Winter' ),
-         ('B0002', '2019-07-22'  ,'00608'  , 'Rhubarb', 'KangaRhu', 'Spring'),
-         ('B0003', '2021-01-10'  ,'00005'  , 'Celery', 'Conquistador', 'Winter'),
-         ('B0004', '2022-01-01'  ,'00473'  , 'Lemon', 'Eureka', 'Spring'),
-         ('B0005', '2018-09-09'  ,'00201'  , 'Lemon', 'Baboon', 'Spring');
+  insert into waste (wasteid, batch, expirationdate, quantity, seedtype, seedname)
+  values ('w001', 'B0001', '2020-01-10'  ,'00125'  , 'Pear', 'Bosc' ),
+         ('w002','B0002', '2019-07-22'  ,'00608'  , 'Rhubarb', 'KangaRhu'),
+         ('w003','B0003', '2021-01-10'  ,'00005'  , 'Celery', 'Conquistador'),
+         ('w004','B0004', '2022-01-01'  ,'00473'  , 'Lemon', 'Eureka'),
+         ('w005', 'B0005', '2018-09-09'  ,'00201'  , 'Lemon', 'Baboon');
+
 
 CREATE TABLE vendor (
     batch CHAR(4) NOT NULL,
@@ -48,13 +50,12 @@ CREATE TABLE vendor (
     PRIMARY KEY (batch, vendorname)
 );
 
-
   insert into vendor (batch, expirationdate, vendorname, seedtype, seedname, quantityavailable)
-  values ('B0021', '2020-01-10'. 'West Coast Seeds' , 'Pear', 'Bosc', 'Winter' ),
-         ('B0054' '2019-07-22', 'Canada Seed Company' , 'Sunflower', 'Titan', 'Spring'),
-         ('B0572', '2021-01-10', 'Farm Direct Seeds' , 'Celery', 'Conquistador', 'Winter'),
-         ('B0300', '2022-01-01' ,'Tropico' , 'Lemon', 'Eureka', 'Spring'),
-         ('B0401', '2018-09-09', 'Tropico'  , 'Grape', 'Moon Drops', 'Spring');
+  values ('B0021', '2020-01-10', 'West Coast Seeds' , 'Pear', 'Bosc', '50000' ),
+         ('B0054' '2019-07-22', 'Canada Seed Company' , 'Sunflower', 'Titan', '73900'),
+         ('B0572', '2021-01-10', 'Farm Direct Seeds' , 'Celery', 'Conquistador', '21204'),
+         ('B0300', '2022-01-01' ,'Tropico' , 'Lemon', 'Eureka', '58208'),
+         ('B0401', '2018-09-09', 'Tropico'  , 'Grape', 'Moon Drops', '10549');
 
 
 
