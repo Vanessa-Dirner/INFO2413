b51a426cf6ccc11c05cfbbd1d859032ca4632e10
Rayalou <97580420+Rayalou@users.noreply.github.com>
2022-02-22 22:51:54 -0800

    Add files via upload

---
+++ LoginPage.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Online_Vegetable_Seed_Management_1_
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        SqlConnection SQLConn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=ASPLogin;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblErrorMessage.Visible = false;
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=ASPLogin;Integrated Security=True;"))
+            {
+                sqlCon.Open();
+                string query = "SELECT COUNT(1) FROM useraccount WHERE username=@username AND password=@password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@username", txtUserName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("password", txtPassword.Text.Trim());
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    Session["username"] = txtUserName.Text.Trim();
+                    Response.Redirect("Dashboard.aspx");
+
+                }
+                else { lblErrorMessage.Visible = true; }
+            }
+
+        }
+    }
+}
