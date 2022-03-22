d2f7c1b5983c3d2005e85e7f05283bf11e7faac4
Rayalou <97580420+Rayalou@users.noreply.github.com>
2022-03-21 21:10:22 -0700

    Create Search Page.aspx.cs

---
+++ Search Page.aspx.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Vegetable_Seeds_Management
+{
+    public partial class Search_Page : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButSearch_Click(object sender, EventArgs e)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            sqlconn.Open();
+            SqlCommand sqlcomm = new SqlCommand();
+            string sqlquery = "select * from inventory where seedName like '%'+@seedName+'%' or plantingTime like '%'+@plantingTime+'%' ";
+            sqlcomm.CommandText = sqlquery;
+            sqlcomm.Connection = sqlconn;
+            sqlcomm.Parameters.AddWithValue("seedName", TxtSeedName.Text);
+            sqlcomm.Parameters.AddWithValue("plantingTime", TxtSeedName.Text);
+            DataTable dt = new DataTable();
+            SqlDataAdapter sda = new SqlDataAdapter(sqlcomm);
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind(); 
+        }
+    }
+}
