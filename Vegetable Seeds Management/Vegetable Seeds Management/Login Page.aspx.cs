4a3c26d22ca49800523984f01f9fca2552af3610
Rayalou <97580420+Rayalou@users.noreply.github.com>
2022-03-22 22:25:19 -0700

    Uploaded files

---
+++ Vegetable Seeds Management/Vegetable Seeds Management/Login Page.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Vegetable_Seeds_Management
+{
+    public partial class Login_Page : System.Web.UI.Page
+    {
+        SqlConnection SQLConn = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB;Initial Catalog=Online vegetable seeds management;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblErrorMessage.Visible = false;
+        }
+
+        protected void Btn_Login(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog = Online vegetable seeds management;Integrated Security=True;"))
+            {
+                sqlCon.Open();
+                string query = "SELECT COUNT(1) FROM staff WHERE userName=@userName AND passwordKey=@passwordKey";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@userName", txtUserName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@passwordKey", txtPassword.Text.Trim());
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    Session["userName"] = txtUserName.Text.Trim();
+                    Response.Redirect("Admin Dashboard.html");
+                }
+                else { lblErrorMessage.Visible = true; }
+            }
+        }
+    }
+}
---
+++ Vegetable Seeds Management/Vegetable Seeds Management/Search Page.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Search Page.aspx.cs" Inherits="Vegetable_Seeds_Management.Search_Page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Searching.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="header">
+            Search <asp:TextBox ID="TxtSeedName" runat="server"></asp:TextBox>
+            <asp:Button ID="ButSearch" runat="server" Text="Button" OnClick="ButSearch_Click" style="height: 26px" />
+
+            <asp:GridView ID="GridView1" runat="server" ></asp:GridView> 
+        </div>
+    </form>
+</body>
+</html>
