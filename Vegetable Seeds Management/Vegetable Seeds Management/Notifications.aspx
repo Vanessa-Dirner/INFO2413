c055f54fb3df97f25c01d7a1edbd1111fa7b4a2f
Vanessa Dirner <vanessa.dirner@email.kpu.ca>
2022-03-23 21:57:23 -0700

    setting up reports page

--- Vegetable Seeds Management/Vegetable Seeds Management/Notifications.aspx
+++ Vegetable Seeds Management/Vegetable Seeds Management/Notifications.aspx
@@ -5,10 +5,16 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+     <link href="LoginPage.css" rel="stylesheet" />
 </head>
 <body>
-    <form id="form1" runat="server">
-        <div>
+    <form id="form3" runat="server">
+        <div class="report">
+            <h2>Generate Reports</h2>
+            <h3>Select the report you'd like to generate:</h3>
+            <asp:RadioButton id="WastedReport" Text="Report Wasted" runat="server" /><br />            
+            <asp:RadioButton id="HarvestedReport" Text="Report Harvested" runat="server" /><br />
+            <asp:button text="Generate!" OnClick="WastedSeedsReport_Click" runat="server"/>
         </div>
     </form>
 </body>
--- Vegetable Seeds Management/Vegetable Seeds Management/Notifications.aspx.cs
+++ Vegetable Seeds Management/Vegetable Seeds Management/Notifications.aspx.cs
@@ -15,22 +15,52 @@ namespace Vegetable_Seeds_Management
         protected void Page_Load(object sender, EventArgs e)
         { }
 
+        protected void WastedSeedsReport_Click(object sender, EventArgs e)
+        {
+
+
+                if (WastedReport.Checked)
+                {
+                    using (SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-UB0LHNH;Initial Catalog=project;Integrated Security=True"))
+
+                    // Get seeds expiring in 1 year or less
+                    sqlCon.Open();
+                    string WastedSeeds = "SELECT * FROM waste order by quantity desc";
+
+                    // notify
+                    if (!string.IsNullOrEmpty(WastedSeeds))
+                    {
+                        Console.WriteLine("The following seeds are expiring within 1 year: ");
+                        Console.WriteLine("expiringSeeds");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No data is available.");
+                    }
+            }
+                else if (HarvestedReport.Checked)
+                {
+                    
+                }
+           
+
+            
+
+        }
+
+    }
+}
+
+
+
+
+/*
+ 
         public class expirynotify
         {
             public expirynotify()
             {
-                using (SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-UB0LHNH;Initial Catalog=project;Integrated Security=True"))
-
-                    // Get seeds expiring in 1 year or less
-                    sqlCon.Open();
-                string expiringSeeds = "SELECT * FROM inventory WHERE expirationdate >= dateadd(year, +1, getdate())";
-                // notify
-                if (string.IsNullOrEmpty(expiringSeeds))
-                {
-                    Console.WriteLine("The following seeds are expiring within 1 year: ");
-                    Console.WriteLine("expiringSeeds");
-                }
-
+                
                 // if seeds have expired, move to expired table
                 /* string expiredSeeds = "SELECT * FROM inventory WHERE expirationdate < getdate()"
 
@@ -40,10 +70,27 @@ namespace Vegetable_Seeds_Management
                          SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
                                  sqlCmd.Parameters.AddWithValue(moveExpiredSeeds)
                      }
-                */
+                
                 // email notification to administrator
 
             }
         }
-    }
-}
+        
+ */
+
+/*
+ * 
+ * using (SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-UB0LHNH;Initial Catalog=project;Integrated Security=True"))
+
+            // Get seeds expiring in 1 year or less
+            sqlCon.Open();
+            string expiringSeeds = "SELECT * FROM inventory WHERE expirationdate >= dateadd(year, +1, getdate())";
+            // notify
+            if (string.IsNullOrEmpty(expiringSeeds))
+            {
+                Console.WriteLine("The following seeds are expiring within 1 year: ");
+                Console.WriteLine("expiringSeeds");
+            }
+
+ * 
+ * */
