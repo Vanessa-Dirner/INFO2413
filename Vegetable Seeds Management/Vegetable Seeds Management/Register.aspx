eb1688cfa41073402bbe8a15da85ea27eb417ad9
Vanessa Dirner <vanessa.dirner@email.kpu.ca>
2022-03-25 00:16:15 -0700

    Creating register page

---
+++ Vegetable Seeds Management/Vegetable Seeds Management/Register.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="Vegetable_Seeds_Management.Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="LoginPage.css" rel="stylesheet" />
+</head>
+<body>
+     <img src="user-01.png" alt="User" class="user" />
+        <h2>Register an Account</h2>
+        <form id="form2" runat="server">
+            <asp:Label Text="Email" CssClass="lblusername" runat="server"></asp:Label>
+            <asp:TextBox ID= "txtUserName" runat="server" CssClass="txtusername"></asp:TextBox>
+            <asp:Label Text="Password" CssClass="lblpassword" runat="server"></asp:Label>
+            <asp:TextBox ID ="txtPassword" runat="server" CssClass="txtpassword"></asp:TextBox>
+            <asp:Button Text="Register" CssClass="btnsubmit" runat="server" OnClick="Btn_Register" />
+            <asp:Label ID="lblErrorMessage" runat="server" Text="Account Created" ForeColor="Red"></asp:Label>
+        </form>
+</body>
+</html>
---
+++ Vegetable Seeds Management/Vegetable Seeds Management/Register.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Vegetable_Seeds_Management
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Userid"] == null) Response.Redirect("Login Page.aspx");
+        }
+
+        protected void Btn_Register(object sender, EventArgs e)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-UB0LHNH;Initial Catalog=project;Integrated Security=True"))
+            {
+                sqlCon.Open();
+                string query = "SELECT COUNT(1) FROM staff WHERE username=@username AND passkey=@passkey";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@username", txtUserName.Text.Trim());
+                sqlCmd.Parameters.AddWithValue("@passkey", txtPassword.Text.Trim());
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    Session["userName"] = txtUserName.Text.Trim();
+                    Response.Redirect("Admin Dashboard.html");
+                }
+                else { lblErrorMessage.Visible = true; }
+
+                Session.Add("Userid", txtUserName.Text);
+            }
+        }
+    }
+}
