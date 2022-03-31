aaefd173d78a079d42644d4834a3396269b3669b
Vanessa Dirner <vanessa.dirner@email.kpu.ca>
2022-03-30 21:43:19 -0700

    updated email notification

--- Vegetable Seeds Management/Vegetable Seeds Management/AutomaticEmails.cs
+++ Vegetable Seeds Management/Vegetable Seeds Management/AutomaticEmails.cs
@@ -7,66 +7,71 @@ using System.Configuration;
 
 namespace Vegetable_Seeds_Management
 {
-    public class SendEmail
+    public class AutomaticEmails
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("Farmer Seeds", "samplefarmseedsco@gmail.com"));
-            message.To.Add(new MailboxAddress("Farmer Seeds", "samplefarmseedsco@gmail.com"));
-            message.Subject = "Vegetable Seed Managment System";
 
-            message.Body = new TextPart("plain")
-            {
-                Text = @"Dear Systems Administrator,
-                Please see the following report(s):
-
-                -- Sent via Systems Managment System
-                "
-            };
-
-            using (var client = new SmtpClient())
-            {
-                client.Connect("smtp.gmail.com", 587, false);
-
-                // Note: only needed if the SMTP server requires authentication
-                client.Authenticate("samplefarmseedsco@gmail.com", "nqlgvacxneplndbt");
-
-                client.Send(message);
-                client.Disconnect(true);
-            }
-        }
-
-
-
-        protected void WastedSeeds(object sender, EventArgs e)
-        {
             using (SqlConnection sqlCon = new SqlConnection(@"Data Source=desktop-ub0lhnh;Initial Catalog=project;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
             {
-                //Get seeds wasted in last year
+                // CHeck for seeds under 500g
                 string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
                 SqlConnection sqlconn = new SqlConnection(mainconn);
                 sqlconn.Open();
                 SqlCommand sqlcomm = new SqlCommand();
-                string WastedSeeds = "SELECT * FROM waste order by quantity desc";
+                string WastedSeeds = "SELECT * FROM waste WHERE  quantity > 500";
                 sqlcomm.CommandText = WastedSeeds;
                 sqlcomm.Connection = sqlconn;
                 DataTable dt = new DataTable();
                 SqlDataAdapter sda = new SqlDataAdapter(sqlcomm);
                 sda.Fill(dt);
 
-
                 // notify
                 if (!string.IsNullOrEmpty(WastedSeeds))
                 {
-                    Console.WriteLine("The following seeds are expiring within 1 year: ");
-                    Console.WriteLine("expiringSeeds");
-                }
-                else
-                {
-                    Console.WriteLine("No data is available.");
+                    string reportmsg = "The following seed quantities are less than 500g";
+                    SendEmail(reportmsg, dt);
                 }
 
+                // Get seeds expiring in 1 year or less
+                sqlCon.Open();
+                string expiringSeeds = "SELECT * FROM inventory WHERE expirationdate >= dateadd(year, +1, getdate())";
+                // notify
+                if (string.IsNullOrEmpty(expiringSeeds))
+                {
+                    MoveExpiredSeeds();
+                    string reportmsg = "The following seeds are expiring in 1 year or less:"
+                    SendEmail(reportmsg, dt);
+                }
+            }
+
+            // use this method to send emails
+            void SendEmail(string reportmsg, DataTable query)
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Farmer Seeds", "samplefarmseedsco@gmail.com"));
+                message.To.Add(new MailboxAddress("Farmer Seeds", "samplefarmseedsco@gmail.com"));
+                message.Subject = "Vegetable Seed Managment System";
+
+                message.Body = new TextPart("plain")
+                {
+                    Text = @"Dear Systems Administrator,
+                Please see the following report(s):
+
+                -- Sent via Systems Managment System
+                "
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect("smtp.gmail.com", 587, false);
+
+                    // Note: only needed if the SMTP server requires authentication
+                    client.Authenticate("samplefarmseedsco@gmail.com", "nqlgvacxneplndbt");
+
+                    client.Send(message);
+                    client.Disconnect(true);
+                }
             }
 
             void MoveExpiredSeeds()
@@ -95,37 +100,10 @@ namespace Vegetable_Seeds_Management
                             DataTable dt1 = new DataTable();
                             SqlDataAdapter sda1 = new SqlDataAdapter(sqlcomm);
                             sda.Fill(dt);
-
-                            //SqlCommand sqlCmd = new SqlCommand(sqlCon);
-                            //sqlCmd.Parameters.AddWithValue(moveExpiredSeeds)
-                        }
-
-                        // email notification to administrator
-                    }
-
-                    void ExpiringSeeds()
-                    {
-
-                        using (SqlConnection sqlConn = new SqlConnection(@"Data Source=desktop-ub0lhnh;Initial Catalog=project;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
-                        {
-
-                            // Get seeds expiring in 1 year or less
-                            sqlCon.Open();
-                            string expiringSeeds = "SELECT * FROM inventory WHERE expirationdate >= dateadd(year, +1, getdate())";
-                            // notify
-                            if (string.IsNullOrEmpty(expiringSeeds))
-                            {
-                                Console.WriteLine("The following seeds are expiring within 1 year: ");
-                                Console.WriteLine("expiringSeeds");
-                            }
-
                         }
                     }
-
-                    MoveExpiredSeeds();
-                    ExpiringSeeds();
-
-
-
                 }
             }
+        }
+    }
+}
